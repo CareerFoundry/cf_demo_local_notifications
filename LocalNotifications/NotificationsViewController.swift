32595da18fee72408fc0896fc31dff8a766f4991
meganjam <megan@careerfoundry.com>
2017-06-02 18:07:12 +0200

    Initial commit

---
+++ LocalNotifications/NotificationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  LocalNotifications
+//
+//  Created by Hesham Abd-Elmegid on 3/7/17.
+//  Copyright © 2017 CareerFoundry. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    @IBAction func requestNotificationsAuthorization(_ sender: Any) {
+        
+    }
+    
+    @IBAction func checkNotificationsAuthorizationStatus(_ sender: Any) {
+
+    }
+    
+    @IBAction func showNotification(_ sender: Any) {
+
+    }
+}
